29ec204be8b1283c78ce8535b1d891949591d2d5
gillupandit07-dot <gillupandit07@gmail.com>
2026-04-17 01:36:34 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+def home():
+    return {"message": "Backend is running"}
+
+@app.post("/submit")
+def submit(data: dict):
+    name = data.get("name")
+    return {"message": f"Hello {name}, data received!"}
